3d826d938b6efedeeac738413691b7d24328fcb9
Maxym Hybalo <maxym.hybalo@masterofcode.com>
2020-09-16 23:56:25 +0300

    initial project setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import 'antd/dist/antd.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Button } from 'antd';
+
+console.log('Try Create new reac view');
+
+ReactDOM.render(
+  <Button type="primary">Button </Button>,
+  document.getElementById('root')
+);
+
