baa8a46b96d30eddb5107d684eef8e19455f2340
Maxym Hybalo <maxym.hybalo@masterofcode.com>
2020-09-21 18:09:27 +0300

    base markup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Button } from 'antd';
+import { Button, Switch, Tabs, Typography, Card } from 'antd';
+
+const { TabPane } = Tabs;
+const { Text } = Typography;
 
 console.log('Try Create new reac view');
 
 ReactDOM.render(
-  <Button type="primary">Button </Button>,
+  <Tabs defaultActiveKey="1" tabPosition="left">
+      <TabPane tab="Tweaks" key="1">
+          <Card bordered={false}>
+            <Text>Blanc links in JIRA</Text>
+            <Switch/>
+          </Card>
+      </TabPane>
+      <TabPane tab="Scripts" key="2"></TabPane>
+      <TabPane tab="Other" key="3"></TabPane>
+      <TabPane tab="Settings" key="4"></TabPane>
+
+  </Tabs>,
   document.getElementById('root')
 );
 
