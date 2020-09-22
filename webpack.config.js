e137e7fcbacc0c7fb10a71b242d88adb855c1bf8
Maxym Hybalo <maxym.hybalo@masterofcode.com>
2020-09-22 17:38:28 +0300

    add eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,51 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
         },
-        {
-            test: /\.html$/,
-            use: [
-              {
-                loader: "html-loader",
-                options: {
-                  minimize: false
-                }
-              }
-            ]
-        },
-        {
-            test: /\.css$/i,
-            use: [
-              {
-                loader: 'style-loader',
-                options: { injectType: 'linkTag' },
-              },
-              { loader: 'file-loader' }
-            ]
-        },
-        {
-          test: /\.scss/,
-          use: [
-            'style-loader',
-            'css-loader',
-            { loader: 'sass-loader'},
-            // { loader: 'file-loader' }
-          ]
-        }
-      ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-          template: "./src/index.html",
-          filename: "./index.html"
-        })
-    ]
-  };
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: false,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: 'style-loader',
+            options: { injectType: 'linkTag' },
+          },
+          { loader: 'file-loader' },
+        ],
+      },
+      {
+        test: /\.scss/,
+        use: [
+          'style-loader',
+          'css-loader',
+          { loader: 'sass-loader' },
+          // { loader: 'file-loader' }
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
+};
