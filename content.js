87c54e35747c74efde38e4b704848bedd174dad4
Maxym Hybalo <maxym.hybalo@masterofcode.com>
2020-10-06 18:16:28 +0300

    listen chane

--- content.js
+++ content.js
@@ -1,5 +1,13 @@
-// console.log('Welcome extension script')
-// eslint-disable-next-line no-undef
-// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+console.log('Welcome extension script');
 
-// });
+function handleJIRA(enable) {
+  const links = document.body.querySelectorAll('a');
+  Array.from(links, (a) => {
+    a.target = enable ? '_blank' : '_target';
+    a.style.color = enable ? '#ddd' : 'inherit';
+    return a;
+  });
+}
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  handleJIRA(request.jira);
+});
--- src/extension/messenger.js
+++ src/extension/messenger.js
@@ -1,9 +1,19 @@
-const sendMessage = () => {
-  console.log('send message');
+const JIRA_TAB_PATTERN = /.atlassian.net/;
+
+function filterTabsByConfig(tabs, config) {
+  let result = [];
+  const { jira } = config;
+  if (jira) result = tabs.filter((tab) => JIRA_TAB_PATTERN.test(tab.url));
+  return result;
+}
+
+const sendMessage = (config) => {
   chrome.tabs.query({ }, (tabs) => {
-    console.log('tabs', tabs);
-    chrome.tabs.sendMessage(tabs[0].id, { greeting: 'hello' }, (response) => {
-      console.log(response);
+    const filtredTabs = filterTabsByConfig(tabs, config);
+    console.log('filtredTabs', filtredTabs);
+
+    filtredTabs.forEach((tab) => {
+      chrome.tabs.sendMessage(tab.id, config, () => {});
     });
   });
 };
