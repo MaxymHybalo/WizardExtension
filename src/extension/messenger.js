c24a770dbb46589cf788d3b1edcf0412d6cbcc25
Maxym Hybalo <maxym.hybalo@masterofcode.com>
2020-10-06 17:25:14 +0300

    rm eslint exceptions

--- src/extension/messenger.js
+++ src/extension/messenger.js
@@ -1,9 +1,7 @@
 const sendMessage = () => {
   console.log('send message');
-  // eslint-disable-next-line no-undef
   chrome.tabs.query({ }, (tabs) => {
     console.log('tabs', tabs);
-    // eslint-disable-next-line no-undef
     chrome.tabs.sendMessage(tabs[0].id, { greeting: 'hello' }, (response) => {
       console.log(response);
     });
