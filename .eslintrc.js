999ad344b5bdb1d166e80190b33ffc54abf77187
Maxym Hybalo <maxym.hybalo@masterofcode.com>
2020-10-12 12:15:18 +0300

    modify rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,9 @@ module.exports = {
     'no-unused-vars': ['error', { varsIgnorePattern: '^React$' }],
     'react/jsx-filename-extension': [1, { extensions: ['js', 'jsx'] }],
     'max-classes-per-file': ['error', 2],
-    'class-methods-use-this': 'off'
+    'class-methods-use-this': 'off',
+    'comma-dangle': 'off',
+    'no-param-reassign': 'off'
   },
   globals: {
     chrome: false,
